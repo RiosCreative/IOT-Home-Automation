5812c801a1292f4d5cfe2bba0c87c7b37f5970f7
Luis Rios <Turego@macbookpro-6035.inside.andrews.edu>
2016-03-23 17:51:06 -0400

    PubNub and Pi connection
    Updated PubNub status indicator to listen for a subscribed message.
    Added code to send messages to RGB LED, Camera, and Temp/Humidity
    sensor on Pi (that corresponds to the Python doc on the Pi).

--- app/src/main/java/com/rioscreative/iotpracticum/ui/MainActivity.java
+++ app/src/main/java/com/rioscreative/iotpracticum/ui/MainActivity.java
@@ -6,17 +6,14 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.*;
+
 import com.pubnub.api.*;
 import com.pubnub.api.Callback;
 import com.rioscreative.iotpracticum.R;
-
-import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -24,11 +21,26 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    // Josh's PubNub connection info
+    // String PUBLISH_KEY = "pub-c-ae8e1c63-7fad-46a3-b5e2-ddd5a799a80b";
+    // String SUBSCRIBE_KEY = "sub-c-e3b3d664-f0a5-11e5-baae-0619f8945a4f";
+    // String channel = "appOut";
+
+    // Luis' PubNub connection info
     String PUBLISH_KEY = "pub-c-5d4c8a5c-b2d4-4974-a11c-4a3fc3c2ad8d";
     String SUBSCRIBE_KEY = "sub-c-d52e3da6-c9e4-11e5-b684-02ee2ddab7fe";
     String channel = "IOTpracticum";
     Pubnub pubnub = new Pubnub(PUBLISH_KEY, SUBSCRIBE_KEY, false);
 
+    // Objects for Devices
+    JSONObject camObj = new JSONObject();
+    JSONObject rgbObj = new JSONObject();
+    JSONObject tempObj = new JSONObject();
+    JSONObject piConnectionCheck = new JSONObject();
+    Object mMessage = new Object();
+
+    // Main Activity Layout connections
     @Bind(R.id.InternetConnectivityLabel) TextView mInternetConnectivityLabel;
     @Bind(R.id.InternetStatusLabel) TextView mInternetStatusLabel;
     @Bind(R.id.PubNubConnectivityLabel) TextView mPubNubConnectivityLabel;
@@ -42,6 +54,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        try {
+            // Capture camera images
+            camObj.put("Type", "CAM");
+            tempObj.put("Type", "SENS");
+            rgbObj.put("Type", "LED");
+            rgbObj.put("RED", 255);
+            rgbObj.put("GREEN", 0);
+            rgbObj.put("BLUE", 0);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
         // If network is available, change InternetStatusLabel text to online and textstyle to statusGreen
         if (isNetworkAvailable()){
@@ -69,8 +95,6 @@ public class MainActivity extends AppCompatActivity {
                             System.out.println("SUBSCRIBE : CONNECT on channel:" + channel
                                     + " : " + message.getClass() + " : "
                                     + message.toString());
-                            mPubNubStatusLabel.setText(R.string.connection_online);
-                            mPubNubStatusLabel.setTextAppearance(R.style.textScheme1_statusGreen);
                         }
 
                         @Override
@@ -90,6 +114,8 @@ public class MainActivity extends AppCompatActivity {
                         public void successCallback(String channel, Object message) {
                             System.out.println("SUBSCRIBE : " + channel + " : "
                                     + message.getClass() + " : " + message.toString());
+                            // Set mMessage equal to a received message from
+                            mMessage = message;
                         }
 
                         @Override
@@ -103,12 +129,29 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        pubnub.publish(channel, "If you receive this, PubNubConnection is working!", callback);
+        // Example publish to device (RGB LED, Camera, or Temp/Humidity sensor);
+        // pubnub.publish(channel, rgbObj, callback);
+        // pubnub.publish(channel, camObj, callback);
+        // pubnub.publish(channel, tempObj, callback);
 
-
+        // Check PubNub Connection
+        pubnub.publish(channel, piConnectionCheck, callback);
+        if (isPubnubAvailable(mMessage)) {
+            mPubNubStatusLabel.setText(R.string.connection_online);
+            mPubNubStatusLabel.setTextAppearance(R.style.textScheme1_statusGreen);
+        }
     }
 
-            private boolean isNetworkAvailable() {
+    // If you receive a message, PubNub is available
+    private boolean isPubnubAvailable(Object message) {
+        boolean isAvailable = false;
+        if (message == mMessage) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private boolean isNetworkAvailable() {
                 ConnectivityManager manager = (ConnectivityManager)
                         getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo networkInfo = manager.getActiveNetworkInfo();
